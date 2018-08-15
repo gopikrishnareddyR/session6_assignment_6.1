9be23b072d5081d6ac3e28542056690cac932314
gopikrishnareddyR <40296528+gopikrishnareddyR@users.noreply.github.com>
2018-08-15 23:54:31 +0530

    Add files via upload

---
+++ session6_assignment_6_1.R
@@ -0,0 +1,69 @@
+#session6_assignment_6.1
+
+#1. Import the Titanic Dataset from the link Titanic Data Set. 
+#Perform the following: 
+
+#a. Preprocess the passenger names to come up with a list of titles that represent families 
+#and represent using appropriate visualization graph.
+
+
+library(ggplot2)
+library(ggthemes)
+library(scales)
+library(dplyr)
+library(mice)
+library(randomForest)
+
+getwd()
+path<-"C:/Users/gopikrishna/Documents/R files test"
+setwd(path)
+library(readr)
+train<-read.csv("C:/Users/gopikrishna/Documents/R files test/train.csv", header=T, sep = ",")
+View(train)
+test<-read.csv("C:/Users/gopikrishna/Documents/R files test/tesst.csv", header=T, sep = ",")
+
+full<-bind_rows(train,test)
+
+full$Fsize <- full$SibSp + full$Parch + 1
+
+strsplit(full$Name, split = '[.,]')[[1]]
+full$surname<-sapply(full$Name, FUN = function(x){strsplit(x,split = '[,.]')[[1]][1]})
+full$Family <- paste(full$surname,full$Fsize, sep='_')
+histogram(full$Fsize,full$surname)
+hist(unique(full$Fsize, incomparables = TRUE))
+
+#b. Represent the proportion of people survived from the family size using a graph. 
+
+
+full$FsizeD[full$Fsize == 1] <- 'single'
+full$FsizeD[full$Fsize <= 4] <- 'small'
+full$FsizeD[full$Fsize >=5] <- 'large'
+
+mosaicplot(table(full$FsizeD , full$Survived), xlim=2,main ='Family Size by Survival', shade=TRUE)
+
+
+#c. Impute the missing values in Age variable using Mice Library, create two different 
+#graphs showing Age distribution before and after imputation. 
+
+summary(full$Age) #The missing age is not randomly distributed across all classes, 
+hist(full$Age)      #but is rather concentrated amongst the passengers from 3rd class,
+g <-ggplot(full,aes(Age))+theme_minimal()+geom_histogram(binwidth=5)
+
+library(mice)
+imp<-mice(as.dataframe(full$Age))
+full$AgeClass = ifelse(full$Age<=10,1,
+                       ifelse(full$Age>10 & full$Age<=20,2,
+                              ifelse(full$Age>20 & full$Age<=35,3,4)))
+full$AgeClass = as.factor(full$AgeClass)
+
+md.pattern(full[,!names(full) %in% c("Survived", "Name", "PassengerId", "Ticket", "AgeClass")])
+
+marginplot(data.frame(full$Age, full$Pclass))
+
+table(full[is.na(full$Age), "Pclass"])
+
+imp$Age <- mice(full[, !names(full) %in% c("Survived", "Name", "PassengerId", "Ticket", "AgeClass", "Cabin", "FamilyName")],m=8,maxit=8,meth='pmm',seed=251863)
+
+hist(imp$Age)
+
+
